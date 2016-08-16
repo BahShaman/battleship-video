3f1f3b799f03f867271d95267af78caf93717a31
Mike B <mikeboscia@gmail.com>
2016-08-15 23:28:46 -0700

    working models and tests

--- lib/BShipGrid.py
+++ lib/BShipGrid.py
@@ -5,19 +5,92 @@ BShipGrid
 class BShipGrid:
 
     _playername = ""
-    _data = array()
+    _data = None
+    _gridrows = 10
+    _gridcols = 10
+    _textoffset = 10
 
     def __init__(self,playername):
         self._playername = playername
+        self._data = [[0 for x in range(self._gridrows)] for y in range(self._gridcols)]
     
-    def getdata():
-        return _data
+    def getdata(self):
+        return self._data
 
-    def setdata(dataarray):
+    def adddata(self,row,col):
+        self.addship(row,col)
+
+    def addship(self,row,col):
+        self._data[row][col]=1
+
+    def addshot(self,row,col):
+        self._data[row][col]=2
+
+    def setdata(self,dataarray):
         self._data = dataarray
     
-    def draw(x,y):
+    def draw(self,drawing,x,y,w,h):
+        self.drawdata(drawing,x,y,w,h)
+        self.drawgrid(drawing,x,y,w,h)
+        self.drawshot(drawing,x,y,w,h)
+        self.drawtext(drawing,x,y,w,h)
+
+    def drawgrid(self,drawing,x,y,w,h):
+        ox = x
+        oy = y
+        gy = x + self._textoffset
+        gx = y + self._textoffset
+        sx = x + w / (self._gridrows+1)
+        sy = y + h / (self._gridcols+1)
+        for i in range(self._gridrows+1):
+            for j in range(self._gridcols+1):
+                #print i,j
+                drawing.line((i*sx+ox+gx, 0+oy+gy, i*sx+ox+gx, j*sy+oy+gy), "blue", 2)
+                drawing.line((0+ox+gx, j*sy+oy+gy, i*sx+ox+gx, j*sy+oy+gy), "blue", 2)
+
+    def drawdata(self,drawing,x,y,w,h):
+        ox = x
+        oy = y
+        gy = x + self._textoffset
+        gx = y + self._textoffset
+        sx = x + w / (self._gridrows+1)
+        sy = y + h / (self._gridcols+1)
         for i in range(10):
             for j in range(10):
-                print i,j
+                #print i,j
+                if self._data[i][j] > 0:
+                    print "SHIP"
+                    drawing.rectangle([(i*sx+ox+gx, j*sy+oy+gy), (i*sx+ox+gx+sx, j*sy+oy+gy+sy)], fill="#666666")
 
+    def drawtext(self,drawing,x,y,w,h):
+        ox = x
+        oy = y
+        gy = x 
+        gx = y 
+        px = -5
+        py = -3
+        sx = x + w / (self._gridrows+1)
+        sy = y + h / (self._gridcols+1)
+        for i in range(self._gridrows+1):
+            for j in range(self._gridcols+1):
+                if not (i == 0 and j == 0):
+                    if i == 0:
+                        drawing.text((i*sx+ox+gx+px, j*sy+oy+gy+py), str(j))
+                    if j == 0:
+                        drawing.text((i*sx+ox+gx+px, j*sy+oy+gy+py), str(chr(i+64)))
+
+    def drawshot(self,drawing,x,y,w,h):
+        ox = x
+        oy = y
+        gy = x + self._textoffset
+        gx = y + self._textoffset
+        px = 4
+        py = 4
+        sx = x + w / (self._gridrows+1)
+        sy = y + h / (self._gridcols+1)
+        for i in range(10):
+            for j in range(10):
+                #print i,j
+                if self._data[i][j] > 1 :
+                    print "HIT"
+                    drawing.ellipse([(i*sx+ox+gx+px, j*sy+oy+gy+py), (i*sx+ox+gx+sx-px, j*sy+oy+gy+sy-py)], fill="#FF0000")
--- unittest/BShipGridBaseTest.py
+++ unittest/BShipGridBaseTest.py
@@ -1,19 +1,58 @@
 import unittest
 import sys
-sys.path.append('../vixen')
-from vixen import Vixen
+sys.path.append('../lib')
 from BShipGrid import BShipGrid
+from PIL import Image, ImageDraw
 
 class BShipGridBaseTest(unittest.TestCase):
-    vixen = None
+    b = None
+    img = None
+    drawing = None
 
     def suite():
         suite = unittest.TestLoader().loadTestsFromTestCase(BShipGridBaseTest)
         return suite
         
     def setUp(self):
-        b = BShipGrid()
-        b.
+        self.b = BShipGrid("Simon")
+        W = 500
+        H = 500
+        self.b.adddata(0,0)
+        self.b.adddata(1,1)
+        self.b.adddata(1,2)
+        self.b.adddata(2,3)
+        self.b.adddata(3,4)
+        self.b.adddata(5,6)
+        self.b.adddata(9,9)
+        self.b.addshot(1,1)
+        self.b.addshot(1,2)
+        self.b.addshot(3,4)
+        self.img = Image.new("RGB", (W, H), (0,255,0,0))
+        self.drawing = ImageDraw.Draw(self.img)
+
+    def test_firsttest(self):
+        self.b.getdata()
+
+    def test_draw(self):
+        self.b.draw(self.drawing,10,10, 100, 100)
+        #self.img.save("test_draw.gif", "gif")#,transparency=0)
+        self.img.save("test_draw.png", "PNG",)
+
+    def test_draw_small(self):
+        self.b.draw(self.drawing,10,10, 50, 50)
+        #self.img.save("test_draw_small.gif", "gif")#,transparency=0)
+        self.img.save("test_draw_small.png", "PNG",)
+
+    def test_draw_large(self):
+        self.b.draw(self.drawing,10,10, 300, 300)
+        #self.img.save("test_draw_large.gif", "gif")#,transparency=0)
+        self.img.save("test_draw_large.png", "PNG",)
+
+    def tearDown(self):
+        self.b = None
+        self.img = None
+        self.drawing = None
+
 
 if __name__ == '__main__':
     unittest.main()
